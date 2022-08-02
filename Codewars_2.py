1a18f75e20a98cefb5537271e349263d63604ecc
AxellePR <Oleksiigolwork@gmail.com>
2022-08-02 10:55:27 +0300

    Second kata added

--- Codewars_2.py
+++ Codewars_2.py
@@ -3,3 +3,13 @@ def sum_two_smallest_numbers(numbers):
     res = sorted(numbers)
     return res[0] + res[1]
 print(sum_two_smallest_numbers(numbers))
+
+def get_middle(str):
+    if len(string) % 2 != 0:
+        middle_letter = int(len(string) / 2)
+        return string[middle_letter:middle_letter + 1]
+    else:
+        two_middle = int(len(string) / 2)
+        return string[two_middle -1 :two_middle  + 1]
+string = 'stoyns'
+print(get_middle(string))
